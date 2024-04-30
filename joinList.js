199ac884a6f4a2e7334fae9f20f1650217cc190d
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-04-30 16:49:47 -0400

    implemented joinList function without using join()

--- joinList.js
+++ joinList.js
@@ -3,14 +3,28 @@
  * into one String, concepts, with each list item separated from
  * the previous by a comma.
  *
- * To implement this we'll create a joinList function which will take 
+ * To implement this we'll create a joinList function which will take
  * in any array of strings return a comma-separated string.
  *
  * Note: We can NOT use the built-in Array join function.
  */
 
 // Write our function (we must define it too!) below
-// ...
+
+const joinList = function(concepts) {
+  if (concepts.length === 0) {
+    return '';
+  }
+  let newList = concepts[0];
+  for (let i = 1; i < concepts.length; i++) {
+    newList += ', ' + concepts[i];
+  }
+  return newList;
+};
+  
+
+  
+
 
 // Test / Driver Code below...
 const conceptList = ["gists", "types", "operators", "iteration", "problem solving"];
